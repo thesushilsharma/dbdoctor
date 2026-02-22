8a39d8ec74fea9616e2c56f63cb578d93b802d8f
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-02-22 07:28:00 +0400

    adding drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   schema: "./src/schema.ts", // Your schema file path
-  out: "./drizzle", // Your migrations folder
+  out: "./src/db/drizzle", // Your migrations folder
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
