f6ab396e278912a4614c13eb32db8bd30e0abbee
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-02-23 06:21:02 +0400

    adding drizzle chnages

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,12 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set")
+}
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
